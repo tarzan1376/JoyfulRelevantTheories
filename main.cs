f73b4c2d11f0114e2f7945e4c183c82a2753cee2
tarzan1376 <tarzan1376@gmail.com>
2024-09-05 13:56:16 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+  public static void Main(string[] args) {
+    Console.WriteLine(Panic()); 
+  }
+
+  public static string Panic() {
+    return @"Oh, well imagine 
+As I'm pacing the pews in a church corridor 
+And I can't help but to hear 
+No, I can't help but to hear an exchanging of words..." + Environment.NewLine;
+  }
+}
+
+
+    
