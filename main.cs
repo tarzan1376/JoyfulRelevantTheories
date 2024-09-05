acc448f73c574efa1ecbe06f9ee5e1187d45d4a1
tarzan1376 <tarzan1376@gmail.com>
2024-09-05 14:05:45 +0000

    updated

--- main.cs
+++ main.cs
@@ -2,14 +2,14 @@ using System;
 
 class Program {
   public static void Main(string[] args) {
-    Console.WriteLine(Panic()); 
+    Console.WriteLine(Lyrics()); 
   }
 
-  public static string Panic() {
+  public static string Lyrics() {
     return @"Oh, well imagine 
 As I'm pacing the pews in a church corridor 
 And I can't help but to hear 
-No, I can't help but to hear an exchanging of words..." + Environment.NewLine;
+No, I can't help but to hear an exchanging of words...";
   }
 }
 
